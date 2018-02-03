b5c893aeb2ab2dbb0369b2f71c8fd9836998dfd1
张三丰 <123456@qq.com>
2018-02-03 10:57:55 +0800

    开启路由完整匹配模式

--- php/application/route.php
+++ php/application/route.php
@@ -24,6 +24,7 @@ use think\Route;
 Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
 //Route::get('api/v1/banner/:id', 'api/v1.Banner/getBanner');
 Route::get('api/:version/theme', 'api/:version.Theme/getSimpleList');
+Route::get('api/:version/theme/:id', 'api/:version.Theme/getComplexOne');
 
 
 //Route::rule('路由表达式', '路由地址', '请求类型', '路由参数（数组）', '变量规则（数组）');
